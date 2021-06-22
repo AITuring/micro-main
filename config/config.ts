d10a1c3033f611765212db1f07806559139f4d42
AITuring <shspku365@gmail.com>
2021-06-23 07:59:28 +0800

    增加shi配置,micro-liao变成了liao

--- config/config.ts
+++ config/config.ts
@@ -7,90 +7,106 @@ import proxy from './proxy';
 import routes from './routes';
 
 const { REACT_APP_ENV } = process.env;
+// TODO 根据环境变量动态配置环境
+const DEV = {
+  alpha: '//micro-alpha.vercel.app:7081',
+  Liao: '//micro-main.vercel.app:7083',
+  PPTist: '//micro-main.vercel.app:7084',
+};
+
+const PRODUCTION = {
+  alpha: 'https://micro-alpha.vercel.app/',
+  Liao: '//micro-main.vercel.app:7083',
+  PPTist: '//micro-main.vercel.app:7084',
+};
 
 export default defineConfig({
-    hash: true,
-    antd: {},
-    dva: {
-        hmr: true,
+  hash: true,
+  antd: {},
+  dva: {
+    hmr: true,
+  },
+  layout: {
+    // https://umijs.org/zh-CN/plugins/plugin-layout
+    locale: true,
+    siderWidth: 208,
+    ...defaultSettings,
+  },
+  // https://umijs.org/zh-CN/plugins/plugin-locale
+  locale: {
+    // default zh-CN
+    default: 'zh-CN',
+    antd: true,
+    // default true, when it is true, will use `navigator.language` overwrite default
+    baseNavigator: true,
+  },
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*',
     },
-    layout: {
-        // https://umijs.org/zh-CN/plugins/plugin-layout
-        locale: true,
-        siderWidth: 208,
-        ...defaultSettings,
+  },
+  // 按需加载
+  dynamicImport: {
+    loading: '@ant-design/pro-layout/es/PageLoading',
+  },
+  // 快速刷新
+  fastRefresh: {},
+  targets: {
+    ie: 11,
+  },
+  // umi routes: https://umijs.org/docs/routing
+  routes,
+  // Theme for antd: https://ant.design/docs/react/customize-theme-cn
+  theme: {
+    'primary-color': defaultSettings.primaryColor,
+  },
+  // esbuild is father build tools
+  // https://umijs.org/plugins/plugin-esbuild
+  esbuild: {},
+  title: false,
+  ignoreMomentLocale: true,
+  proxy: proxy[REACT_APP_ENV || 'dev'],
+  manifest: {
+    basePath: '/',
+  },
+  openAPI: [
+    {
+      requestLibPath: "import { request } from 'umi'",
+      // 或者使用在线的版本
+      // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
+      schemaPath: join(__dirname, 'oneapi.json'),
+      mock: false,
     },
-    // https://umijs.org/zh-CN/plugins/plugin-locale
-    locale: {
-        // default zh-CN
-        default: 'zh-CN',
-        antd: true,
-        // default true, when it is true, will use `navigator.language` overwrite default
-        baseNavigator: true,
+    {
+      requestLibPath: "import { request } from 'umi'",
+      schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
+      projectName: 'swagger',
     },
-    devServer: {
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-    },
-    // 按需加载
-    dynamicImport: {
-        loading: '@ant-design/pro-layout/es/PageLoading',
-    },
-    // 快速刷新
-    fastRefresh: {},
-    targets: {
-        ie: 11,
-    },
-    // umi routes: https://umijs.org/docs/routing
-    routes,
-    // Theme for antd: https://ant.design/docs/react/customize-theme-cn
-    theme: {
-        'primary-color': defaultSettings.primaryColor,
-    },
-    // esbuild is father build tools
-    // https://umijs.org/plugins/plugin-esbuild
-    esbuild: {},
-    title: false,
-    ignoreMomentLocale: true,
-    proxy: proxy[REACT_APP_ENV || 'dev'],
-    manifest: {
-        basePath: '/',
-    },
-    openAPI: [
+  ],
+  qiankun: {
+    master: {
+      apps: [
         {
-            requestLibPath: "import { request } from 'umi'",
-            // 或者使用在线的版本
-            // schemaPath: "https://gw.alipayobjects.com/os/antfincdn/M%24jrzTTYJN/oneapi.json"
-            schemaPath: join(__dirname, 'oneapi.json'),
-            mock: false,
+          name: 'alpha',
+          entry: 'https://micro-alpha.vercel.app/',
+        },
+        // {
+        //   name: 'avatar-gen',
+        //   entry: '//localhost:7082',
+        // },
+        {
+          name: 'Liao',
+          entry: 'https://liao.vercel.app/',
         },
         {
-            requestLibPath: "import { request } from 'umi'",
-            schemaPath: 'https://gw.alipayobjects.com/os/antfincdn/CA1dOm%2631B/openapi.json',
-            projectName: 'swagger',
+          name: 'PPTist',
+          entry: 'https://micro-ppt-ist.vercel.app/',
         },
-    ],
-    qiankun: {
-        master: {
-            apps: [
-                {
-                    name: 'alpha',
-                    entry: 'https://micro-alpha.vercel.app/',
-                },
-                // {
-                //   name: 'avatar-gen',
-                //   entry: '//localhost:7082',
-                // },
-                {
-                  name: 'Liao',
-                  entry: 'https://micro-liao.vercel.app/',
-                },
-                {
-                  name: 'PPTist',
-                  entry: 'https://micro-ppt-ist.vercel.app/',
-                },
-            ],
+        {
+          name: 'shi',
+          entry: 'https://shi.vercel.app/',
         },
+      ],
     },
+  },
 });
--- config/routes.ts
+++ config/routes.ts
@@ -1,76 +1,75 @@
 ﻿export default [
-    {
+  {
+    path: '/user',
+    routes: [
+      {
         path: '/user',
         routes: [
-            {
-                path: '/user',
-                routes: [
-                    {
-                        name: 'login',
-                        path: '/user/login',
-                        component: './user/Login',
-                    },
-                ],
-            },
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
         ],
-    },
-    {
-        name: 'alpha',
-        icon: 'CrownTwoTone',
-        path: '/alpha',
-        microApp: 'alpha',
-    },
-//    没有子应用，没用了
-//     {
-//       name: 'avatar-gen',
-//       icon: 'IdcardTwoTone',
-//       path: '/avatar-gen',
-//       microApp: 'avatar-gen',
-//     },
-    {
-      name: 'Liao',
-      icon: 'CompassTwoTone',
-      path: '/Liao',
-      microApp: 'Liao',
-    },
-    {
-      name: 'PPTist',
-      icon: 'FilePptTwoTone',
-      path: '/PPTist',
-      microApp: 'PPTist',
-    },
-    {
-        path: '/welcome',
-        name: 'welcome',
-        icon: 'smile',
+      },
+    ],
+  },
+  {
+    name: 'alpha',
+    icon: 'CrownTwoTone',
+    path: '/alpha',
+    microApp: 'alpha',
+  },
+  {
+    name: 'Liao',
+    icon: 'CompassTwoTone',
+    path: '/Liao',
+    microApp: 'Liao',
+  },
+  {
+    name: 'shi',
+    icon: 'TrophyTwoTone',
+    path: '/shi',
+    microApp: 'Liaoshi',
+  },
+  {
+    name: 'PPTist',
+    icon: 'FilePptTwoTone',
+    path: '/PPTist',
+    microApp: 'PPTist',
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'FlagTwoTone',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'CrownTwoTone',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'SmileTwoTone',
         component: './Welcome',
-    },
-    {
-        path: '/admin',
-        name: 'admin',
-        icon: 'crown',
-        access: 'canAdmin',
-        component: './Admin',
-        routes: [
-            {
-                path: '/admin/sub-page',
-                name: 'sub-page',
-                icon: 'smile',
-                component: './Welcome',
-            },
-        ],
-    },
-    {
-        name: 'list.table-list',
-        icon: 'table',
-        path: '/list',
-        component: './TableList',
-    },
-    {
-        path: '/',
-        redirect: '/welcome',
-    },
-    {
-        component: './404',
-    },
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'AppstoreTwoTone',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    component: './404',
+  },
 ];
